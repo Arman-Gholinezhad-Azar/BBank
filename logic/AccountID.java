0d3353a3e78861b94899f328aa1ee089121a5a1f
Arman <cybertechik@protonmail.com>
2026-09-18 12:45:16 +0330

    finished account id object logic

--- logic/AccountID.java
+++ logic/AccountID.java
@@ -1,4 +1,43 @@
 package logic;
 
-public class AccountID {
+import java.util.Objects;
+import java.util.UUID;
+
+public final class AccountID {
+
+    private final UUID value;
+
+    public AccountID(UUID uuid) {
+        this.value = Objects.requireNonNull(uuid, "UUID cannot be null!");
+    }
+
+    public static AccountID generate() {
+        return new AccountID(UUID.randomUUID());
+    }
+
+    public static AccountID fromString(String s) {
+        return new AccountID(UUID.fromString(s));
+    }
+
+    public UUID getValue() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountID that = (AccountID) o;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "AccountID{" + value + '}';
+    }
 }
